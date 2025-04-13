47becfdb3c50d86dd83167838df109f50d366418
Igor Voronkov <115886661+Tyulenb@users.noreply.github.com>
2025-04-13 13:38:40 +0300

    Add non-neg slack del_fsm

--- laba2/del_fsm.v
+++ laba2/del_fsm.v
@@ -7,7 +7,6 @@ module del_fsm(
     output reg r_o
     );
     
-    wire [8:0] exp;
 
     
     reg [24:0] divisible;
@@ -16,13 +15,16 @@ module del_fsm(
     reg [7:0] divd_exp;
     reg divd_fb;
     reg divs_fb;
-    reg [3:0] state = 0;
+    reg [4:0] state = 0;
     
     reg [4:0] iter = 0;
     
     reg [23:0] mant_res = 0;
     reg [24:0] reminder = 0;
     reg [4:0] carry = 0;
+    reg [4:0] carry_sh = 0;
+    reg [8:0] exp;
+    reg [31:0] res_reg = 0;
     
     initial
     begin
@@ -38,12 +40,15 @@ module del_fsm(
         mant_res = 0;
         reminder = 0;
         carry = 0;
+        carry_sh = 0;
+        exp = 0;
+        res_reg = 0;
     end
     
     always@(posedge clk)
     begin
         case(state)
-            4'b0000:
+            5'b00000:
             begin
                 divisible <= 0;
                 divider <= 0;
@@ -54,10 +59,11 @@ module del_fsm(
                 iter <= 0;
                 mant_res <= 0;
                 reminder <= 0;
-                carry = 0;
-                state <= 4'b0001;
+                carry <= 0;
+                res_reg <= 0;
+                state <= 5'b00001;
             end
-            4'b0001: if(r_i)
+            5'b00001: if(r_i)
             begin
                 divisible <= {1'b1, n[22:0]};
                 divider <= {1'b1, x[22:0]};
@@ -65,110 +71,132 @@ module del_fsm(
                 divd_exp <= x[30:23];
                 divd_fb <= n[31];
                 divs_fb <= x[31];
-                state <= 4'b0010;
+                state <= 5'b00010;
             end
-            4'b0010:
+            5'b00010:
             begin
-                mant_res <= divisible/divider;
-                state <= 4'b0011;
+                if(divisible >= divider)
+                begin
+                    divisible <= divisible-divider;
+                    mant_res <= mant_res +1'b1;
+                    state <= 5'b00010;
+                end
+                else
+                begin
+                    reminder <= divisible;
+                    state <= 5'b00011;
+                end
             end
-            4'b0011:
-            begin
-                reminder <= (divisible%divider); 
-                state <= 4'b0100;
-            end
-            4'b0100:
+            5'b00011:
             begin
                 reminder <= reminder << 1;
-                state <= 4'b0101;
+                state <= 5'b00100;
             end
-            4'b0101:
+            5'b00100:
             begin
                 if(!mant_res[23])
-                    state <= 4'b0110;
+                    state <= 5'b00101;
                 else
-                    state <= 4'b1010; //normalizing step
+                    state <= 5'b01010; //normalizing step
             end
-            4'b0110:
+            5'b00101:
             begin
                 if(reminder < divider)
                 begin
                     mant_res <= mant_res << 1;
                     reminder <= reminder << 1;
-                    state <= 4'b1001;
+                    state <= 5'b01001;
                 end
                 else
-                    state <= 4'b0111;
+                    state <= 5'b00110;
             end
-            4'b0111:
+            5'b00110:
             begin
-                mant_res <= (mant_res << 1) + reminder/divider;
-                state <= 4'b1000;
+                mant_res <= (mant_res << 1) + 1'b1;
+                state <= 5'b00111;
             end
-            4'b1000:
+            5'b00111:
             begin
-                reminder <= (reminder - divider) << 1;
-                state <= 4'b1001;
+                reminder <= reminder - divider;
+                state <= 5'b01000;
             end
-            4'b1001:
+            5'b01000:
+            begin
+                reminder <= reminder << 1;
+                state <= 5'b01001;
+            end
+            5'b01001:
             begin
                 if(iter < 22)
                 begin
                     iter <= iter + 1;
-                    state <= 4'b0101;
+                    state <= 5'b00100;
                 end
                 else
                 begin
-                    state <= 4'b1010;
+                    state <= 5'b01010;
                     iter <= 0;
                 end
             end
-            4'b1010: //normalizing
+            5'b01010: //normalizing
             begin
                 if(iter <= 23)
-                    state <= 4'b1011;
+                    state <= 5'b01011;
                 else
-                    state <= 4'b1100;
+                    state <= 5'b01100;
             end
-            4'b1011:
+            5'b01011:
             begin
                 if(mant_res[iter])
                 begin
                     carry <= iter;
-                    state <= 4'b1101;
+                    state <= 5'b01101;
                 end
                 else
-                    state <= 4'b1101;
+                    state <= 5'b01101;
             end
-            4'b1101:
+            5'b01101:
             begin
                 iter <= iter + 1;
-                state <= 4'b1010;
+                state <= 5'b01010;
             end
-            4'b1100:
+            5'b01100:
             begin
-                mant_res <= mant_res << (23 - carry);
-                state <= 4'b1110;
+                carry_sh <= 23 - carry;
+                state <= 5'b01110;
             end
-            
-            4'b1110:
+            5'b01110:
             begin
-                state <= 4'b0000;
+                mant_res <= mant_res << carry_sh;
+                state <= 5'b01111;
+            end
+            5'b01111:
+            begin
+                exp <= {1'b0,divs_exp}+7'b1111111-{1'b0,divd_exp};
+                state <= 5'b10000;
+            end
+            5'b10000:
+            begin
+                res_reg <= {divs_fb^divd_fb, exp[7:0]-23+carry, mant_res[22:0]};
+                state <= 5'b10001;
+            end
+            5'b10001:
+            begin
+                state <= 5'b00000;
             end
             default:
-                state <= 4'b0000;
+                state <= 5'b00000;
         endcase
     end
     
     always@(posedge clk)
     begin
-        if(state == 4'b1110)
+        if(state == 5'b10001)
             r_o = 1;
         else
             r_o = 0;
     end    
     
-    assign exp = {1'b0,divs_exp}+7'b1111111-{1'b0,divd_exp};
-    assign res = {divs_fb^divd_fb, exp[7:0]-23+carry, mant_res[22:0]};
+    assign res = res_reg;
     
 endmodule
